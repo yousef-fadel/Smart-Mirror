346a35e82d0341f8cbb04258d741f99e75928350
yousef-fadel <yousef_758@outlook.com>
2024-11-10 17:14:08 +0200

    Add LCD_I2C header file

---
+++ lcd_i2c.h
@@ -0,0 +1,22 @@
+#ifndef LCD_I2C_H
+#define LCD_I2C_H
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
+
+#define LCD_CHARACTER  1
+#define LCD_COMMAND    0
+
+#define MAX_LINES      2
+#define MAX_CHARS      16
+
+void i2c_write_byte(uint8_t val);
+
+void lcd_init();
+void lcd_set_cursor(int line, int position);
+void lcd_string(const char *s);
+void lcd_clear(void);
+#endif
