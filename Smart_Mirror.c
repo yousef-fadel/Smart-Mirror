d8ef37f06371ca143afcef96735b878256e2602d
yousef-fadel <yousef_758@outlook.com>
2024-11-10 17:21:55 +0200

    Add wifi library and change code to test serial monitor

--- Smart_Mirror.c
+++ Smart_Mirror.c
@@ -1,43 +1,15 @@
 #include <stdio.h>
 #include "lcd_i2c.h"
+#include "pico/cyw43_arch.h"
 
 
 
 int main()
 {
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-    #warning i2c/lcd_1602_i2c example requires a board with I2C pins
-#else
-    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    stdio_init_all();
+    while(1){
 
-    lcd_init();
-
-    static char *message[] =
-            {
-                    "RP2040 by", "Raspberry Pi",
-                    "A brand new", "microcontroller",
-                    "Twin core M0", "Full C SDK",
-                    "More power in", "your product",
-                    "More beans", "than Heinz!"
-            };
-
-    while (1) {
-        for (uint m = 0; m < sizeof(message) / sizeof(message[0]); m += MAX_LINES) {
-            for (int line = 0; line < MAX_LINES; line++) {
-                lcd_set_cursor(line, (MAX_CHARS / 2) - strlen(message[m + line]) / 2);
-                lcd_string(message[m + line]);
-            }
-            sleep_ms(2000);
-            lcd_clear();
-        }
+    printf("hi");
+    sleep_ms(3000);
     }
-#endif
-
 }
