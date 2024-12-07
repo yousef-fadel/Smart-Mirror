100c9669fcbbbcf8e0ab3dbc06254ed8431c2564
yousef-fadel <yousef_758@outlook.com>
2024-12-07 10:46:51 +0200

    Add FreeRTOS

--- Smart_Mirror.c
+++ Smart_Mirror.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include "lcd_i2c.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "wiper.h"
-#include "light_sensor.h"
-#include "ir_sensor.h"
+#include "sensors/light_sensor.h"
+#include "sensors/ir_sensor.h"
 #include "led.h"
-#include "ultra_sonic_sensor.h"
+#include "sensors/ultra_sonic_sensor.h"
 
 volatile bool mirror_timer_finished = false;
 void init()
@@ -51,7 +54,7 @@ int main()
     lcd_set_cursor(0, 0);
     while (1)
     {
-        int mirrorActivation = read_ultra_sonic(ACTIVATE_MIRROR_IR_SENSOR);
+        int mirrorActivation = 1;
         if (mirrorActivation)
         {
             add_alarm_in_ms(10000, mirror_on_timer, NULL, true);
